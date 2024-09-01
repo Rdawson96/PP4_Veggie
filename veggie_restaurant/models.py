fd4befcb21fccb3046598d96ef71fcfdaaffe17e
Rob Dawson <rdawson164@gmail.com>
2024-09-01 21:42:30 +0000

    added home page basic

--- veggie_restaurant/models.py
+++ veggie_restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_guests = models.IntegerField()
+    special_requests = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"Booking for {self.name} on {self.date} at {self.time}"
