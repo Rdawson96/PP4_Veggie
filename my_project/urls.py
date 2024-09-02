2c02a0bce7c911211b2d457824fabf8850468aa1
Rob Dawson <rdawson164@gmail.com>
2024-09-02 02:54:04 +0000

    added login sign up and logout pages and added content to home page and wiring up routes

--- my_project/urls.py
+++ my_project/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from veggie_restaurant import views as veggie_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', veggie_views.home, name='home'),  # Home page
     path('menu/', veggie_views.menu, name='menu'),  # Menu page
     path('booking/', veggie_views.booking, name='booking'),  # Booking page
+     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), # Log in page
+    path('signup/', veggie_views.signup, name='signup'), # Sign up page
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'), # Logout page
 ]
--- veggie_restaurant/views.py
+++ veggie_restaurant/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -8,5 +11,17 @@ def home(request):
 def menu(request):
     return render(request, 'menu.html')
 
+@login_required
 def booking(request):
     return render(request, 'booking.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
