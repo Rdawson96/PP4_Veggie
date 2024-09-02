56f094b4588ab2d5db74d489a91a4a1c90abf5ff
Rob Dawson <rdawson164@gmail.com>
2024-09-02 03:13:15 +0000

    chaged nav bar buttons and altered views.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -98,6 +98,8 @@ CSRF_TRUSTED_ORIGINS = [
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = 'login'
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
--- veggie_restaurant/views.py
+++ veggie_restaurant/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from .models import Booking
 
 def home(request):
     return render(request, 'home.html')
@@ -13,7 +12,33 @@ def menu(request):
 
 @login_required
 def booking(request):
-    return render(request, 'booking.html')
+    user_bookings = Booking.objects.filter(user=request.user).order_by('-date', '-time')
+
+    if request.method == 'POST':
+        # Capture form data
+        name = request.POST['name']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
+        date = request.POST['date']
+        time = request.POST['time']
+        number_of_guests = request.POST['number_of_guests']
+        special_requests = request.POST.get('special_requests', '')
+
+        # Create and save the new booking
+        new_booking = Booking.objects.create(
+            user=request.user,
+            name=name,
+            email=email,
+            phone_number=phone_number,
+            date=date,
+            time=time,
+            number_of_guests=number_of_guests,
+            special_requests=special_requests
+        )
+
+        return redirect('booking')
+
+    return render(request, 'booking.html', {'user_bookings': user_bookings})
 
 def signup(request):
     if request.method == 'POST':
